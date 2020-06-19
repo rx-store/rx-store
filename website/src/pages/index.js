d4d69ebe381fed351fa4d689662f38e0e37fa2d6
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-06-18 22:47:22 -0700

    updates taglines

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -54,8 +54,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="The stream management library">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
