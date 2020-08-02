785bf1958b9d1cbd1b442cb880e0bf5c186d2d29
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-08-02 13:06:14 -0700

    mapped types for sinks, hoist to core

--- libs/react-rx-store/src/lib/react-rx-store.tsx
+++ libs/react-rx-store/src/lib/react-rx-store.tsx
@@ -7,7 +7,7 @@ import React, {
 } from 'react';
 import { tap } from 'rxjs/operators';
 import { Observable, Subject } from 'rxjs';
-import { RxStoreEffect } from '@rx-store/rx-store';
+import { RxStoreEffect, createSinks } from '@rx-store/rx-store';
 
 /**
  * A React hook that consumes from the passed Rx Store context,
@@ -30,19 +30,6 @@ const createSources = <T extends {}>(debugKey: string, value: T) => {
             console.log(`${debugKey} source ${subjectName}:`, value)
           )
         ),
-    }),
-    {}
-  ) as T;
-};
-
-const createSinks = <T extends {}>(debugKey: string, value: T) => {
-  return Object.keys(value).reduce(
-    (acc, subjectName) => ({
-      ...acc,
-      [subjectName]: (...args) => {
-        console.log(`${debugKey} sink ${subjectName}: `, ...args);
-        value[subjectName].next(...args);
-      },
     }),
     {}
   ) as T;
--- libs/rx-store/src/lib/rx-store.ts
+++ libs/rx-store/src/lib/rx-store.ts
@@ -1,8 +1,25 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+
+export type Sinks<T extends { [k: string]: Subject<any> }> = {
+  [P in keyof T]?: T[P]['next'];
+};
+
+export const createSinks = <T extends {}>(debugKey: string, value: T) => {
+  return Object.keys(value).reduce(
+    (acc, subjectName) => ({
+      ...acc,
+      [subjectName]: (...args) => {
+        console.log(`${debugKey} sink ${subjectName}: `, ...args);
+        value[subjectName].next(...args);
+      },
+    }),
+    {}
+  ) as Sinks<T>;
+};
 
 /** A function that will subscribe to an effect */
 export type RxStoreEffect<T> = (
   sources: T,
-  sinks: T,
+  sinks: Sinks<T>,
   runEffect: (debugKey: string, effect: RxStoreEffect<T>) => Observable<any>
 ) => Observable<any>;
