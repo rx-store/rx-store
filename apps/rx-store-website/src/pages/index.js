684e5921b20e8c00c52b9997f9377d84b725635a
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-08-17 09:22:32 -0700

    docs(adds devtools blurb, rewords features, adds carbon code sample)

--- apps/rx-store-website/src/pages/index.js
+++ apps/rx-store-website/src/pages/index.js
@@ -5,31 +5,41 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import carbon from './carbon.png';
 
 const features = [
   {
     title: <>High Performance</>,
-
-    description: (
-      <>Only render/deliver updates for what changed, nothing more.</>
-    ),
-  },
-  {
-    title: <>Reactive</>,
     description: (
       <>
-        The primitive source of truth are individual event emitters. Combine
-        them to build "up" streams instead of "paring down" state (like with
-        other state managers).
+        <p>
+          Pushes updates for specific changes, unlike other state managers that
+          run every reducer on every action, and render every component on every
+          change.
+        </p>
+        <p>
+          As a metaphor, this is like receiving a push notification that wakes
+          your phone up from sleeping instead of wasting power &amp; data
+          polling all the time.
+        </p>
       </>
     ),
   },
   {
-    title: <>Functional</>,
+    title: <>Functional Reactive Programming</>,
     description: (
       <>
-        Abstract away the dimension of time, using the full power of RxJS.
-        Declare complex async logic with pure functions.
+        <p>
+          Unlike other reactive state managers, Rx Store does not try to make
+          imperative code reactive. All code you will be writing in Rx Store can
+          be free of mutable shared state, making it easier to test, debug,
+          reason about, and share your code.
+        </p>
+        <p>
+          State and side effects aren't strictly separated, and there is no
+          dogmatic boilerplate to write, however you are free to layer &amp;
+          structure your business logic in this way if desired.
+        </p>
       </>
     ),
   },
@@ -38,7 +48,7 @@ const features = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx('col col--6', styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -73,6 +83,7 @@ function Home() {
               Get Started
             </Link>
           </div>
+          <img src={carbon} />
         </div>
       </header>
       <main>
@@ -92,14 +103,49 @@ function Home() {
             <div className="row">
               <div className="container">
                 <div className="row" style={{ justifyContent: 'center' }}>
-                  <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube-nocookie.com/embed/pI6ALDSD-QE"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
-                  ></iframe>{' '}
+                  <div className={clsx('col col--6', styles.feature)}>
+                    <h3>Devtools Sneak Peak</h3>
+                    <p>
+                      Rx Store is able to know, prior to subscribing, which
+                      effects consume from which subjects, which subjects each
+                      effect can emit onto, and anytime an effect spawns an
+                      inner effect, anytime that errors or completes, and it
+                      knows when an effect receives a value from whence subject
+                      it came as well as the value, and it knows exactly when
+                      effects emit and onto which subjects.
+                    </p>
+                    <p>
+                      This is all made possible via curried sources, sinks, and
+                      spawnEffect, and a recursive tree like structuring of
+                      effects allowing Rx Store to keep track of a "stack trace"
+                    </p>
+                    <p>
+                      Each instance of the effect is tracked, and as the data
+                      flows in & out we are able to track it. Consumers can wrap
+                      inner observables eg. with switchMap() using the
+                      spwanEffect() helper, and Rx Store will be able to track
+                      where an effect was spawned from all the way up to the
+                      root, like a stack trace, example:
+                    </p>
+                  </div>
+                  <div className={clsx('col col--6', styles.feature)}>
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube-nocookie.com/embed/pI6ALDSD-QE"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen
+                    ></iframe>
+                    Due to Rx Store's unique architecture, the entire store is
+                    introspectable. This affordance allows for hot new features
+                    such as the interactive 3D data flow visualization. It can
+                    be used to filter, throttle, and coalesce logs, as well as
+                    &nbsp;
+                    <a href="https://github.com/rx-store/rx-store/pull/48">
+                      other powerful features that are in the works
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
