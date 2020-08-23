6d11db488daf70ba957c97a812f47e6c42d5c4f1
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-08-23 13:22:52 -0700

    restores counter example

--- apps/react-example-counter/src/app/App.tsx
+++ apps/react-example-counter/src/app/App.tsx
@@ -1,16 +1,15 @@
 import Counter from './Counter';
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 
 const App: React.FC<{}> = () => {
-  // const [n, setN] = useState(1);
-  const n = 1;
+  const [n, setN] = useState<number>(1);
   return (
     <>
-      {/* {new Array(n).fill(null).map((_, i) => ( */}
-      <Counter />
-      {/* ))} */}
-      {/* <button onClick={}>add a counter</button>
-      <button onClick={() => setN(n - 1)}>remove a counter</button> */}
+      {new Array(n).fill(null).map((_, i) => (
+        <Counter key={i} />
+      ))}
+      <button onClick={() => setN(n + 1)}>add a counter</button>
+      <button onClick={() => setN(n - 1)}>remove a counter</button>
     </>
   );
 };
