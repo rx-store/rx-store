3fa9a16f375e373162c825206699f3f5a924ca6f
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-08-25 09:41:40 -0700

    creates tests

--- libs/react/src/lib/react.spec.tsx
+++ libs/react/src/lib/react.spec.tsx
@@ -1,3 +1,32 @@
-it('', () => {
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { store, useStore, useSubscription } from './react';
+import { BehaviorSubject } from 'rxjs';
+import { act } from 'react-dom/test-utils';
+
+it('legacy signature', () => {
   expect(true).toBe(true);
+});
+
+it('new signature', () => {
+  const value = { count$: new BehaviorSubject(0) };
+  const { Manager, context } = store({
+    value,
+  });
+  const MyComponent: React.FC<{}> = () => {
+    const store = useStore(context);
+    const [count] = useSubscription(store.count$);
+    return <div role="main">{count}</div>;
+  };
+  render(
+    <Manager>
+      <MyComponent />
+    </Manager>
+  );
+  expect(screen.getByRole('main')).toHaveTextContent('0');
+  act(() => {
+    value.count$.next(1);
+  });
+  expect(screen.getByRole('main')).toHaveTextContent('1');
 });
