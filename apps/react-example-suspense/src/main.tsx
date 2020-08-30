36be5323ae5e3b96c9f6488f1c3e1de3ad00cb67
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-08-29 21:03:43 -0700

    creates a suspense example

--- apps/react-example-suspense/src/main.tsx
+++ apps/react-example-suspense/src/main.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from 'react-experimental';
+import ReactDOM from 'react-dom-experimental';
+import { StoreValue, Effect } from '@rx-store/core';
+import { store } from '@rx-store/react';
+import { BehaviorSubject, Subject, interval } from 'rxjs';
 import App from './app/app';
+import { Suspense } from 'react';
+import { scan } from 'rxjs/operators';
 
-ReactDOM.render(
+interface AppStoreValue extends StoreValue {
+  count$: Subject<number>;
+}
+
+const storeValue: AppStoreValue = {
+  count$: new BehaviorSubject(0),
+};
+
+const effect: Effect<AppStoreValue> = ({ sources, sinks }) =>
+  interval(1000).pipe(sinks.count$());
+
+const { Manager, context } = store({ value: storeValue, effect });
+export const rootContext = context;
+
+ReactDOM.unstable_createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Manager>
+      <App />
+    </Manager>
+  </React.StrictMode>
 );
