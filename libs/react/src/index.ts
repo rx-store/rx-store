0971591207db8e8a9b4b03e0946cba18005ab86a
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-09-07 11:47:17 -0700

    feat(hoists all re-exports to top level)

--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -1,2 +1,5 @@
-export * from './lib/react';
 export { Manager } from './lib/manager';
+export { useStore } from './lib/use-store';
+export { useSubscription } from './lib/use-subscription';
+export { withSubscription } from './lib/with-subscription';
+export { store, StoreArg, StoreReturn } from './lib/store';
--- libs/react/src/lib/react.tsx
+++
@@ -1,4 +0,0 @@
-export { useStore } from './use-store';
-export { useSubscription } from './use-subscription';
-export { withSubscription } from './with-subscription';
-export { store, StoreArg, StoreReturn } from './store';
