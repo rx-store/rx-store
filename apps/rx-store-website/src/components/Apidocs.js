fcdb5243aa9e52ca2d17ba9e416420c2896c5142
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-09-07 11:56:46 -0700

    docs(adds padding on typedoc pages)

--- apps/rx-store-website/src/components/Apidocs.js
+++ apps/rx-store-website/src/components/Apidocs.js
@@ -6,7 +6,7 @@ export default ({ __content, __menu }) => {
     <Layout title="API docs">
       <div class="container">
         <div class="row">
-          <div class="col col--8">
+          <div class="col col--8" style={{ paddingTop: 15, paddingBottom: 15 }}>
             <div dangerouslySetInnerHTML={{ __html: __content }} />
           </div>
           <div class="col col--4">
